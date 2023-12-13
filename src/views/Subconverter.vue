35565a1e2542aeb2200258410bdad4d10d8029cc
Stone <74167978+s71557@users.noreply.github.com>
2023-12-13 17:21:44 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -208,107 +208,12 @@ export default {
           ssr: "ssr",
           ssd: "ssd",
           ClashR: "clashr",
-          Surge2: "surage&ver=2",
+          Surge2: "surge&ver=2",
         },
         backendOptions: [{ value: "http://127.0.0.1:25500/sub?" }],
         remoteConfig: [
           {
-            label: "ACL4SSR",
-            options: [
-              {
-                label: "不良林解决DNS泄露 无分流群组(与Github同步)",
-                value:
-                  "https://raw.githubusercontent.com/s71557/ACL4SSR/master/Clash/config/nodnsleak.ini"
-              },
-              {
-                label: "ACL4SSR_Online 默认版 分组比较全(与Github同步)",
-                value:
-                  "https://raw.githubusercontent.com/s71557/ACL4SSR/master/Clash/config/ACL4SSR_Online.ini"
-              },
-              {
-                label: "ACL4SSR_Online_AdblockPlus 更多去广告(与Github同步)",
-                value:
-                  "https://raw.githubusercontent.com/s71557/ACL4SSR/master/Clash/config/ACL4SSR_Online_AdblockPlus.ini"
-              },
-              {
-                label: "ACL4SSR_Online_MultiCountry 多国分组(与Github同步)",
-                value:
-                  "https://raw.githubusercontent.com/s71557/ACL4SSR/master/Clash/config/ACL4SSR_Online_MultiCountry.ini"
-              },
-              {
-                label: "ACL4SSR_Online_NoAuto 无自动测速(与Github同步)",
-                value:
-                  "https://raw.githubusercontent.com/s71557/ACL4SSR/master/Clash/config/ACL4SSR_Online_NoAuto.ini"
-              },
-              {
-                label: "ACL4SSR_Online_NoReject 无广告拦截规则(与Github同步)",
-                value:
-                  "https://raw.githubusercontent.com/s71557/ACL4SSR/master/Clash/config/ACL4SSR_Online_NoReject.ini"
-              },
-              {
-                label: "ACL4SSR_Online_Mini 精简版(与Github同步)",
-                value:
-                  "https://raw.githubusercontent.com/s71557/ACL4SSR/master/Clash/config/ACL4SSR_Online_Mini.ini"
-              },
-              {
-                label: "ACL4SSR_Online_Mini_AdblockPlus.ini 精简版 更多去广告(与Github同步)",
-                value:
-                  "https://raw.githubusercontent.com/s71557/ACL4SSR/master/Clash/config/ACL4SSR_Online_Mini_AdblockPlus.ini"
-              },
-              {
-                label: "ACL4SSR_Online_Mini_NoAuto.ini 精简版 不带自动测速(与Github同步)",
-                value:
-                  "https://raw.githubusercontent.com/s71557/ACL4SSR/master/Clash/config/ACL4SSR_Online_Mini_NoAuto.ini"
-              },
-              {
-                label: "ACL4SSR_Online_Mini_Fallback.ini 精简版 带故障转移(与Github同步)",
-                value:
-                  "https://raw.githubusercontent.com/s71557/ACL4SSR/master/Clash/config/ACL4SSR_Online_Mini_Fallback.ini"
-              },
-              {
-                label: "ACL4SSR_Online_Mini_MultiMode.ini 精简版 自动测速、故障转移、负载均衡(与Github同步)",
-                value:
-                  "https://raw.githubusercontent.com/s71557/ACL4SSR/master/Clash/config/ACL4SSR_Online_Mini_MultiMode.ini"
-              },
-              {
-                label: "ACL4SSR_Online_Mini_MultiCountry.ini 精简版 带港美日国家(与Github同步)",
-                value:
-                  "https://raw.githubusercontent.com/s71557/ACL4SSR/master/Clash/config/ACL4SSR_Online_Mini_MultiCountry.ini"
-              },
-              {
-                label: "ACL4SSR_Online_Full 全分组 重度用户使用(与Github同步)",
-                value:
-                  "https://raw.githubusercontent.com/s71557/ACL4SSR/master/Clash/config/ACL4SSR_Online_Full.ini"
-              },
-              {
-                label: "ACL4SSR_Online_Full_MultiMode.ini 全分组 多模式 重度用户使用(与Github同步)",
-                value:
-                  "https://raw.githubusercontent.com/s71557/ACL4SSR/master/Clash/config/ACL4SSR_Online_Full_MultiMode.ini"
-              },
-              {
-                label: "ACL4SSR_Online_Full_NoAuto.ini 全分组 无自动测速 重度用户使用(与Github同步)",
-                value:
-                  "https://raw.githubusercontent.com/s71557/ACL4SSR/master/Clash/config/ACL4SSR_Online_Full_NoAuto.ini"
-              },
-              {
-                label: "ACL4SSR_Online_Full_AdblockPlus 全分组 重度用户使用 更多去广告(与Github同步)",
-                value:
-                  "https://raw.githubusercontent.com/s71557/ACL4SSR/master/Clash/config/ACL4SSR_Online_Full_AdblockPlus.ini"
-              },
-              {
-                label: "ACL4SSR_Online_Full_Netflix 全分组 重度用户使用 奈飞全量(与Github同步)",
-                value:
-                  "https://raw.githubusercontent.com/s71557/ACL4SSR/master/Clash/config/ACL4SSR_Online_Full_Netflix.ini"
-              },
-              {
-                label: "ACL4SSR_Online_Full_Google 全分组 重度用户使用 谷歌细分(与Github同步)",
-                value:
-                  "https://raw.githubusercontent.com/s71557/ACL4SSR/master/Clash/config/ACL4SSR_Online_Full_Google.ini"
-              }
-            ]
-          },
-          {
-              label: "universal",
+            label: "universal",
             options: [
               {
                 label: "No-Urltest",
@@ -325,11 +230,6 @@ export default {
           {
             label: "customized",
             options: [
-              {
-                label: "仅自用勿选择",
-                value:
-                  "http://192.168.11.252/ACL4SSR_Online_Full_Google.ini"
-              },
               {
                 label: "Maying",
                 value:
@@ -757,10 +657,10 @@ export default {
     },
     getBackendVersion() {
       this.$axios
-        。get(
+        .get(
           defaultBackend.substring(0, defaultBackend.length - 5) + "/version"
         )
-        。then(res => {
+        .then(res => {
           this.backendVersion = res.data.replace(/backend\n$/gm, "");
           this.backendVersion = this.backendVersion.replace("subconverter", "");
         });
